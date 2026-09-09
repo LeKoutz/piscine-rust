323cb8f4c3c8276a4d0b2652fcf450693bd4fb17
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-09 20:52:44 +0300

    DATA - strings

---
+++ strings/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn char_length(s: &str) -> usize {
+    s.chars().count()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_ascii() {
+        let s = "ascii";
+        assert_eq!(char_length(s), 5);
+    }
+
+    #[test]
+    fn test_emoji() {
+        let s = "❤😍";
+        assert_eq!(char_length(s), 2);
+    }
+    #[test]
+    fn test_chinese_char() {
+        let s = "形声字";
+        assert_eq!(char_length(s), 3);
+    }
+}
