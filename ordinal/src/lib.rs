5271e44a717699036f4d6ff939b4535b0aae81a6
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-16 14:00:40 +0300

    PATTERNS - ordinal

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    let ordinal = match x % 10 {
+        1 if x % 100 != 11 => "st",
+        2 if x % 100 != 12 => "nd",
+        3 if x % 100 != 13 => "rd",
+        _ => "th"
+    };
+    format!("{}{}", x, ordinal)
+}
