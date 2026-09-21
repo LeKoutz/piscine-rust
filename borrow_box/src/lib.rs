81aa401d324a206211592941821c3cf73fdfb295
Georgios Koutzos <koutzosgiorgos@gmail.com>
2026-09-21 11:52:24 +0300

    POINTERS - borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u32),
+    pub p2: (String, u32),
+    pub nb_games: u32,
+}
+
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
+        GameSession{
+            id,
+            p1: (p1_name, 0),
+            p2: (p2_name, 0),
+            nb_games,
+        }
+    }
+
+    pub fn read_winner(&self) -> Option<&(String, u32)> {
+        if self.p1.1 > self.p2.1 {
+            Some(&self.p1)
+        } else if self.p2.1 > self.p1.1 {
+            Some(&self.p2)
+        } else {
+            None
+        }
+    }
+
+    pub fn update_score(&mut self, user_name: &str) {
+        let score_diff = self.p1.1.max(self.p2.1) - self.p1.1.min(self.p2.1);
+        let remaining_games = self.nb_games - (self.p1.1 + self.p2.1);
+        if score_diff < remaining_games {
+            if self.p1.0 == user_name {
+                self.p1.1 += 1;
+            } else if self.p2.0 == user_name {
+                self.p2.1 += 1;
+            }
+        }
+    }
+
+    pub fn delete(self) -> String {
+        format!("game deleted: id -> {}", self.id)
+    }
+}
